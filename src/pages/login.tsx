ecacb9e2e72b9ca2020ab79f3f02678bcb2d7785
Shakti242 <shaktipatel2421@gmail.com>
2024-12-05 21:07:06 -0500

    Add login functionality

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useRef } from "react";
-import { Link } from "react-router-dom";
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button';
 import {
   Card,
   CardContent,
@@ -9,56 +7,84 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { login } from '@/http/api';
+import useTokenStore from '@/store';
+import { useMutation } from '@tanstack/react-query';
+import { LoaderCircle } from 'lucide-react';
+import { useRef } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
-const LoginForm = () => {
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const setToken = useTokenStore((state) => state.setToken);
+
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => {
+      navigate('/dashboard/home');
+    },
+  });
+
   const handleLoginSubmit = () => {
     const email = emailRef.current?.value;
     const password = passwordRef.current?.value;
-  }
+    console.log('data', { email, password });
+
+    if (!email || !password) {
+      return alert('Please enter email and password');
+    }
+
+    mutation.mutate({ email, password });
+  };
   return (
     <section className="flex justify-center items-center h-screen">
-
-      <Card className="mx-auto max-w-sm">
+      <Card className="w-full max-w-sm">
         <CardHeader>
           <CardTitle className="text-2xl">Login</CardTitle>
           <CardDescription>
-            Enter your email below to login to your account
+            Enter your email below to login to your account. <br />
+            {mutation.isError && (
+              <span className="text-red-500 text-sm">{'Something went wrong'}</span>
+            )}
           </CardDescription>
         </CardHeader>
-        <CardContent>
-          <div className="grid gap-4">
-            <div className="grid gap-2">
-              <Label htmlFor="email">Email</Label>
-              <Input ref={emailRef}
-                id="email"
-                type="email"
-                placeholder="m@example.com"
-                required
-              />
-            </div>
-            <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Password</Label>
-                <Link to="#" className="ml-auto inline-block text-sm underline">
-                  Forgot your password?
-                </Link>
-              </div>
-              <Input ref={passwordRef} id="password" type="password" required />
-            </div>
+        <CardContent className="grid gap-4">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input
+              ref={emailRef}
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              required
+            />
+          </div>
+          <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input ref={passwordRef} id="password" type="password" required />
           </div>
         </CardContent>
         <CardFooter>
           <div className="w-full">
-            <Button onClick={handleLoginSubmit} className="w-full"> Sign in</Button>
+            <Button
+              onClick={handleLoginSubmit}
+              className="w-full"
+              disabled={mutation.isPending}>
+              {mutation.isPending && <LoaderCircle className="animate-spin" />}
+
+              <span className="ml-2">Sign in</span>
+            </Button>
+
             <div className="mt-4 text-center text-sm">
-              Don't have an account?{''}
+              Don't have an account?{' '}
               <Link to={'/auth/register'} className="underline">
-                Sign in
+                Sign up
               </Link>
             </div>
           </div>
@@ -68,5 +94,4 @@ const LoginForm = () => {
   );
 };
 
-
-export default LoginForm;
+export default LoginPage;
